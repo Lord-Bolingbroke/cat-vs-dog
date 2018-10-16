c725aff1e7d0d582f484b507c2e836bc93bcea7d
Noah Schooler and Tavish OKeefe <noah@>
2018-10-16 15:55:33 -0700

    add pictures of cat and dog in JS

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
     $("ul#cat").prepend("<li>Meow meow</li>");
     $("ul#dog").prepend("<li>Woof woof</li>");
   });
+
+  $("#cat-pic").click(function() {
+    $("#cat-pic").after('<img src="img/cat.jpeg">');
+  });
+
+  $("#dog-pic").click(function() {
+    $("#dog-pic").after('<img src="img/dog.jpeg">');
+  });
 });
